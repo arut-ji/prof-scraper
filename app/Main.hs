ea13841e30dc6e881521314cd735e2b6949e01a2
Arut Jinadit <arut.jinadit@gmail.com>
2020-11-17 23:35:20 +0000

    [Added]: implementation of the first task.

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,21 @@
 module Main where
 
-import Text.HTML.Scalpel
 import ProfScrapeLib
+import Text.HTML.Scalpel
 
 schools :: [String]
-schools = [
-          "chemistry",
-          "computing",
-          "engineering",
-          "ges",
-          "mathematicsstatistics",
-          "physics",
-          "psychology"
-         ]
+schools =
+  [ "chemistry",
+    "computing",
+    "engineering",
+    "ges",
+    "mathematicsstatistics",
+    "physics",
+    "psychology"
+  ]
 
 main :: IO ()
 main = do
-     counts <- mapM numProfessors schools
-     let results = zip schools counts
-     mapM_ (\x -> putStrLn $ (fst x) ++ ": " ++ ((show.snd) x)) results
-
+  counts <- mapM numProfessors schools
+  let results = zip schools counts
+  mapM_ (\x -> putStrLn $ fst x ++ ": " ++ (show . snd $ x)) results
--- src/ProfScrapeLib.hs
+++ src/ProfScrapeLib.hs
@@ -1,8 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module ProfScrapeLib
-    (  numProfessors
-    ) where
+  ( numProfessors,
+  )
+where
+
+import Data.List
+import Text.HTML.Scalpel
 
 numProfessors :: String -> IO (Maybe Int)
-numProfessors x = return Nothing
+numProfessors x = do
+  res <- scrapeURL (constructURL x) staffs
+  return $ length . filterProfessors . concat <$> res
+
+constructURL :: String -> String
+constructURL x = "https://www.gla.ac.uk/schools/" ++ x ++ "/staff"
+
+staffs :: Scraper String [[String]]
+staffs = chroots ("ul" @: [nonHonoraryVisiting]) $ texts "li"
+  where
+    nonHonoraryVisiting = notP ("id" @= "honorary-visitinglist")
+
+filterProfessors :: [String] -> [String]
+filterProfessors = filter pred
+  where
+    pred x = isProf x && isNotAssistantProf x && isNotAssociateProf x
+    isProf = isInfixOf "Professor"
+    isNotAssistantProf = not . isInfixOf "Assistant Professor"
+    isNotAssociateProf = not . isInfixOf "Associate Professor"
